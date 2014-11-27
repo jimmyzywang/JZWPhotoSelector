8756f5dc0f297405b6affa5bf45388b8627ec337
jimmyzywang <jimmyzywang@qq.com>
2014-11-27 20:41:52 +0800

    init proj
    init proj

---
+++ JZWPhotoSelector/JZWPhotoSelector/Others/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JZWPhotoSelector
+//
+//  Created by jimmyzywang-nb on 14/11/27.
+//  Copyright (c) 2014年 com.JZWang.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JZWPhotoSelector/JZWPhotoSelector/Others/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JZWPhotoSelector
+//
+//  Created by jimmyzywang-nb on 14/11/27.
+//  Copyright (c) 2014年 com.JZWang.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
